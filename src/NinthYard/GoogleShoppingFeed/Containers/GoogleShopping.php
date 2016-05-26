acfbdaeb0d6b34284e73d518af7074cc2f3b5b11
Elliot Reed <elliot@universal-textiles.com>
2016-05-26 18:58:57 +0100

    Fixed composer.json

--- src/NinthYard/GoogleShoppingFeed/Containers/GoogleShopping.php
+++ src/NinthYard/GoogleShoppingFeed/Containers/GoogleShopping.php
@@ -6,37 +6,33 @@ use NinthYard\GoogleShoppingFeed\Feed;
 class GoogleShopping
 {
 
-	/**
-	 * [$containers description]
-	 * @var array
-	 */
+    /**
+     * [$containers description]
+     * @var array
+     */
+    public static $container = null;
 
-	public static $container = null;
+    /**
+     * [container description]
+     * @return [type] [description]
+     */
+    public static function container()
+    {
+        if (is_null(static::$container)) {
+            static::$container = new Feed;
+        }
+        return static::$container;
+    }
 
-	/**
-	 * [container description]
-	 * @return [type] [description]
-	 */
-
-	public static function container()
-	{
-		if (is_null(static::$container))
-		{
-			static::$container = new Feed;
-		}
-		return static::$container;
-	}
-
-	/**
-	 * [__callStatic description]
-	 * @param  [type] $name      [description]
-	 * @param  [type] $arguments [description]
-	 * @return [type]            [description]
-	 */
-
-	public static function __callStatic($name, $arguments)
-	{
-		return call_user_func_array(array(static::container(), $name), $arguments);
-	}
+    /**
+     * [__callStatic description]
+     * @param  [type] $name      [description]
+     * @param  [type] $arguments [description]
+     * @return [type]            [description]
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        return call_user_func_array(array(static::container(), $name), $arguments);
+    }
 
 }
